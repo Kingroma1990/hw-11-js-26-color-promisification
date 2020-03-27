ad39e0a991cd28c20d2262c4edf2fb85d0e74819
Roma <king_roma@ukr.net>
2020-03-27 22:20:32 +0300

    3

--- js/index2.js
+++ js/index2.js
@@ -11,7 +11,7 @@ const users = [
  * Должно работать так
  */
 const toggleUserState = (allUsers, userName) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(resolve => {
     const updatedUsers = allUsers.map(user =>
       user.name === userName ? { ...user, active: !user.active } : user,
     );
